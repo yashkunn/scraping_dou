bcaa0d0358b554b3988ae3d64ee6b77f96e94aa9
Yakiv Lobach <lobachyakiv@gmail.com>
2024-11-14 10:30:56 +0200

    Add fields to DouItem class

--- dou/items.py
+++ dou/items.py
@@ -7,6 +7,8 @@ import scrapy
 
 
 class DouItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    title = scrapy.Field()
+    company = scrapy.Field()
+    salary = scrapy.Field()
+    description = scrapy.Field()
+    technologies = scrapy.Field()
